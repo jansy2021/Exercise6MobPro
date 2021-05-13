d3b78fa4d3b37f3b57ff4b99e96406cc0e204ae8
Jansy Imoliana <s11420020@student.unklab.ac.id>
2021-05-13 12:07:06 +0800

    upload tugas6 terlambat

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import Registration from './components/registration/index.js';
+
+const App = () => {
+  return (
+    <View>
+      <Registration/>
+    </View>
+  );
+};
+
+export default App;
